095212030976b9ca8b24385bd0b3077732d48b62
HienTran <hientm0978@gmail.com>
2024-10-22 20:09:25 +0700

    Delete Src/Koi_Pond_Construction_Ordering_System directory

--- Src/Koi_Pond_Construction_Ordering_System/Koi_Pond_Construction_Ordering_System/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Koi_Pond_Construction_Ordering_System.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
