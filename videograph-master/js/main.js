1dfd55cf534889c95b37b0eaf60c7ecf228c5ecd
SeaCow2735 <thisisaseacow@gmail.com>
2024-11-04 22:05:38 +0700

    thay đổi nội dung by Nhân

--- videograph-master/js/main.js
+++ videograph-master/js/main.js
@@ -167,16 +167,16 @@
     /*------------------
         Counter
     --------------------*/
-    $('.counter_num').each(function () {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 4000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
+    // $('.counter_num').each(function () {
+    //     $(this).prop('Counter', 0).animate({
+    //         Counter: $(this).text()
+    //     }, {
+    //         duration: 4000,
+    //         easing: 'swing',
+    //         step: function (now) {
+    //             $(this).text(Math.ceil(now));
+    //         }
+    //     });
+    // });
 
 })(jQuery);
