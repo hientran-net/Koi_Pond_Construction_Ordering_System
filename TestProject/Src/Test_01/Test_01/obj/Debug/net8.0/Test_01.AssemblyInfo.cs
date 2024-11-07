f7bb150b8b950516e8fa8f578c2469a85ef8d66c
HienTran <hientm0978@gmail.com>
2024-11-07 22:28:35 +0700

    Hien - test thay đổi layout admin chỉnh sửa, các button,...

--- TestProject/Src/Test_01/Test_01/obj/Debug/net8.0/Test_01.AssemblyInfo.cs
+++ TestProject/Src/Test_01/Test_01/obj/Debug/net8.0/Test_01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a4287c04d59aa3cb4ccbbc03f92618cc69f7f39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
