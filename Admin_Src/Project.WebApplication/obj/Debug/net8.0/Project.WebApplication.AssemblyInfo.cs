e6b18d7c7d59b6102d728df37c8b33507164ba1f
HienTran <hientm0978@gmail.com>
2024-11-19 11:24:08 +0700

    Hien - fix hoàn thiện các chức năng

--- Admin_Src/Project.WebApplication/obj/Debug/net8.0/Project.WebApplication.AssemblyInfo.cs
+++ Admin_Src/Project.WebApplication/obj/Debug/net8.0/Project.WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6bae8c31b246f802f3ffd8e057c10eb43cc133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b865d377e0762f17b3e1ba27e38e8e0891f2c7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
